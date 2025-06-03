dff7b9632246abc5f2c077a735621142056fc91a
Georgy <georgiisafonk@gmail.com>
2025-06-03 20:55:15 +0700

    new reader categories added

--- library-server-app/src/main.py
+++ library-server-app/src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.app.api.readers import reader_router
 from src.app.api.librarians import librarian_router
@@ -9,3 +10,16 @@ app = FastAPI(title="Library Fund Server")
 app.include_router(reader_router)
 app.include_router(librarian_router)
 app.include_router(literature_router)
+
+origins = [
+    "http://localhost:5173",  # адрес фронтенда
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # или ["*"] для всех, но лучше конкретно фронтенд
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
